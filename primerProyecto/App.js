cfeef59db061715684f1770591bf1a6c5f54f9ca
yocoendi78 <92427812+yocoendi@users.noreply.github.com>
2022-11-09 14:21:53 +0100

    d

--- primerProyecto/App.js
+++ primerProyecto/App.js
@@ -1,130 +1,127 @@
 import React, { useState } from "react";
-import { StyleSheet, Button, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Button, Text, View, TextInput,Switch } from 'react-native';
 
 
 
 export default function App() {
-  const [booleano, setBooleano] = useState(false);
-
-  const [nombre, setNombre] = useState();
-  const [apellidos, setApellidos] = useState();
-  const [edad, setEdad] = useState();
-  const [coreo, setCorreo] = useState();
-
-
-  const [texto, setText] = useState();
+    const [isEnabled, setIsEnable] = useState(null);
+    const [nombre, setNombre] = useState();
+    const [apellidos, setApellidos] = useState();
+    const [edad, setEdad] = useState();
+    const [coreo, setCorreo] = useState();
+    const [sexo, setSexo] = useState();
+    const [texto, setText] = useState();
 
 
 
-  const printText = () => {
+    const printText = () => {
 
-    setText(
+        setText(
 
-      <Text style={{ flexDirection: 'row' }}>
+            <Text style={{ flexDirection: 'row' }}>
 
-        <Text>mi nombre es {nombre} {apellidos}con edad {edad}, y mi correo es  {coreo} </Text>
+                <Text>Mi nombre es {nombre} {apellidos} tengo {edad} años, y mi correo es {coreo} {isEnabled? <Text>Hombre</Text>:<Text>Mujer</Text>}</Text>
 
-      </Text>
+            </Text>
 
-    )
+        )
 
-  }
-  
-  return (
-    <View>
+    }
 
-
-      <View style={{ flexDirection: 'row' }}>
-        <Text style={styles.texto}>Nombre</Text>
-        <TextInput
-        style={styles.input}
-        placeholder="Nombre"
-        keyboardType="name-phone-pad"
-        onChangeText={nombre => setNombre(nombre)}
-      />
-      </View>
- 
-      <View style={{ flexDirection: 'row' }}>
-        <Text style={styles.texto}>Apellidos</Text>
-        <TextInput
-        style={styles.input}
-        placeholder="Apellidos"
-        keyboardType="default"
-        onChangeText={apellidos => setApellidos(apellidos)}
-      />
-      </View>
-      
-      <View style={{ flexDirection: 'row' }}>
-        <Text style={styles.texto}>Edad</Text>
-        <TextInput
-        style={styles.input}
-        placeholder="Edad"
-        keyboardType="numeric"
-        onChangeText={edad => setEdad(edad)}
-      />
-      </View>
-
-      <View style={{ flexDirection: 'row' }}>
-        <Text style={styles.texto}>Correo</Text>
-        <TextInput
-        style={styles.input}
-        placeholder="correo"
-        keyboardType="email-address"
-        onChangeText={correo => setCorreo(correo)}
-      />
-      </View>
-      
+    return (
+        <View>
 
 
+            <View style={{ flexDirection: 'row' }}>
+                <Text style={styles.texto}>Nombre</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Nombre"
+                    keyboardType="name-phone-pad"
+                    onChangeText={nombre => setNombre(nombre)}
+                />
+            </View>
 
+            <View style={{ flexDirection: 'row' }}>
+                <Text style={styles.texto}>Apellidos</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Apellidos"
+                    keyboardType="default"
+                    onChangeText={apellidos => setApellidos(apellidos)}
+                />
+            </View>
 
+            <View style={{ flexDirection: 'row' }}>
+                <Text style={styles.texto}>Edad</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Edad"
+                    keyboardType="numeric"
+                    onChangeText={edad => setEdad(edad)}
+                />
+            </View>
 
+            <View style={{ flexDirection: 'row' }}>
+                <Text style={styles.texto}>Correo</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="correo"
+                    keyboardType="email-address"
+                    onChangeText={correo => setCorreo(correo)}
+                />
+                
+                <Switch
+                    trackColor={{ false: 'red', true: 'blue' }}
+                    thumbColor={isEnabled ? 'blue' : 'red'}
+                    onValueChange={() => setIsEnabled((previousState) => !previousState)}
+                    value={isEnabled}
+                />
 
-
-
-
-
-   
-      
-      <View style={styles.botones}>
-        <Button
-          onPress={printText}
-          title={"Enviar"}
-        />
-       <Text>{texto}</Text>
-      </View>
-    </View>
-  );
+            </View>
 
 
 
- 
+
+            <View style={styles.botones}>
+                <Button
+                    onPress={printText}
+                    title={"Enviar"}
+                />
+                <Text>{texto}</Text>
+            </View>
+        </View>
+    );
+
+
+
+
 }
 
 
 
 const styles = StyleSheet.create({
-  texto: {
-    color: 'black',
-    fontSize: 20,
-    paddingTop: 50
-  },
-  textoNombre: {
-    color: 'blue',
-    fontSize: 20,
-    paddingTop: 50
-  },
-  botones: {
-    paddingTop: 20,
-    paddingBottom: 20,
-    paddingLeft: 30,
-    paddingRight: 30,
-    borderRadius: 200,
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  }
+    texto: {
+        color: 'black',
+        fontSize: 20,
+        paddingTop: 50
+    },
+    textoNombre: {
+        color: 'blue',
+        fontSize: 20,
+        paddingTop: 50
+    },
+    botones: {
+        paddingTop: 20,
+        paddingBottom: 20,
+        paddingLeft: 30,
+        paddingRight: 30,
+        borderRadius: 200,
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 14,
+    }
 })
