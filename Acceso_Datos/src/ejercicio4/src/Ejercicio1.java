55b5ae355b01b14cb6d064577e53ec10a666734b
yocoendi78 <92427812+yocoendi@users.noreply.github.com>
2022-11-11 09:12:24 +0100

    d

--- Acceso_Datos/src/ejercicio4/src/Ejercicio1.java
+++ Acceso_Datos/src/ejercicio4/src/Ejercicio1.java
@@ -1,43 +1,38 @@
-
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-
-		 LocalDateTime now = LocalDateTime.now();
-		 
-		 System.out.println("�Cual es su nombre?");
 		 
 		 Scanner sc = new Scanner(System.in);
 		 
 		 String name = sc.nextLine();
 		 
-	
-		String resultado = "";
-		
-		if (now.getHour() > 0 && now.getHour() <= 12) {
-			
-		System.out.println( resultado="Buenos dias " + name);   
-		
-		}else if (now.getHour() > 12 && now.getHour() < 20) {
-			
-		System.out.println(resultado="Buenos tardes " + name);
-			
-		}else if (now.getHour() > 21 && now.getHour() < 00) {
-			
-			System.out.println(resultado="Buenos noches " + name);
-			
-		}
-		
+		 horaDia("¿Cual es su nombre?");
 
+	}
+		
+	public static horaDia (String mensaje) {
+
+		LocalDateTime now = LocalDateTime.now();
+		String resultado = "";
+	
+	if (now.getHour() > 0 && now.getHour() <= 12) {
+		
+	System.out.println( resultado="Buenos dias " + name);   
+	
+	}else if (now.getHour() > 12 && now.getHour() < 20) {
+		
+	System.out.println(resultado="Buenos tardes " + name);
+		
+	}else if (now.getHour() > 21 && now.getHour() < 00) {
+		
+		System.out.println(resultado="Buenos noches " + name);
 		
 	}
-			
 
-		
+	}
+
 
 }
